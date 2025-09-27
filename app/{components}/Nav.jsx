e7fccb05e3eb0cac716f790f5e3a5f5790e0e17b
Jyothish007 <jyothishaji2002@gmail.com>
2025-09-27 12:18:28 +0530

    [Add] logout and middleware

--- app/{components}/Nav.jsx
+++ app/{components}/Nav.jsx
@@ -1,8 +1,17 @@
-import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons";
+"use client";
+import {
+  faHome,
+  faSignOut,
+  faTicket,
+  faUserCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 const Nav = () => {
+  const { data: session } = useSession();
   return (
     <nav className="flex justify-between bg-nav p-4">
       <div className="flex items-center space-x-4">
@@ -13,8 +22,16 @@ const Nav = () => {
           <FontAwesomeIcon icon={faTicket} className="icon" />
         </Link>
       </div>
-      <div>
-        <p className="text-default-text">jyothish@gmail.com</p>
+      <div className="flex">
+        <FontAwesomeIcon icon={faUserCircle} className="icon pr-2" />
+        <p className="text-default-text">{session?.user?.email}</p>
+        <Link href="/api/auth/signout">
+          <FontAwesomeIcon
+            icon={faSignOut}
+            onClick={() => signOut()}
+            className="icon"
+          />
+        </Link>
       </div>
     </nav>
   );
